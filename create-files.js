a1503de42a6de8a42168edaafdafa219f45d27b3
Anthony Chu <anthony.chu@microsoft.com>
2021-04-02 23:58:07 +0000

    Delete files, update files, add files

--- create-files.js
+++ create-files.js
@@ -4,7 +4,7 @@ const folder = "web/files"
 const fs = require("fs")
 const path = require("path")
 
-for (let i = 1000; i < 3000; i++) {
+for (let i = 2900; i < 3200; i++) {
     fs.writeFileSync(path.join(folder, `${i}.txt`), lorem.generateWords(10000))
 }
 
