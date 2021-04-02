a196030c2894d243c22013167905a69bb72fd36c
Anthony Chu <anthony@anthonychu.ca>
2021-04-02 16:04:27 -0700

    Update script

--- validate-files.js
+++ validate-files.js
@@ -1,9 +1,9 @@
-const fetch = require("node-fetch-retry")
+const fetch = require("node-fetch").default
 var streamEqual = require('stream-equal')
 const fs = require('fs')
 const path = require('path')
 
-const baseUrl = "https://thankful-desert-0bca1c01e.azurestaticapps.net"
+const baseUrl = "https://witty-desert-01bfde800.azurestaticapps.net"
 // const baseUrl = "http://localhost:4280"
 const baseFolder = "web"
 
@@ -11,18 +11,24 @@ async function validate() {
     const folder = path.join(baseFolder, "files")
     const files = fs.readdirSync(folder)
 
+    const errors = []
+
     for (const file of files) {
         const filePath = path.join(folder, file)
         if (!fs.lstatSync(filePath).isFile()) continue
 
-        const res = await fetch(`${baseUrl}/files/${file}`, { method: 'GET', retry: 3, timeout: 2000 })
-        const bodyStream = res.body
-        const fileStream = fs.createReadStream(filePath)
-        const result = await streamEqual(bodyStream, fileStream)
-        fileStream.close()
+        const res = await fetch(`${baseUrl}/files/${file}`)
+        const bodyStream = await res.text()
+        const fileStream = fs.readFileSync(filePath, 'utf-8')
+        const result = bodyStream === fileStream
+        // fileStream.close()
+        // bodyStream.destroy()
         
+        if (!result) errors.push(file)
         console.log(filePath, result)
     }
+
+    console.log(JSON.stringify(errors, null, 2))
 }
 
 validate()
